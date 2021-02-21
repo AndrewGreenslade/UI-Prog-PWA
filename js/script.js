197389f8bdc70f70a203f2b02b11a7879e46a564
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2021-02-21 15:18:55 +0000

    changed melee event listiners

--- js/script.js
+++ js/script.js
@@ -171,7 +171,8 @@ document.getElementById("buttonLeft").onmouseup = function() {ButtonUp()};
 document.getElementById("buttonRight").onmouseup = function() {ButtonUp()};
 document.getElementById("MeleeButton").onmouseup = function() {MeleeButtonUp()};
 
-document.getElementById("MeleeButton").addEventListener("touchend", MeleeButtonDown, {passive: true});
+document.getElementById("MeleeButton").addEventListener("touchstart", MeleeButtonDown, {passive: true});
+document.getElementById("MeleeButton").addEventListener("touchend", MeleeButtonUp, {passive: true});
 
 document.getElementById("buttonUp").addEventListener("touchstart", UpbuttonOnClick, {passive: true});
 document.getElementById("buttonUp").addEventListener("touchend", ButtonUp);
@@ -184,7 +185,6 @@ document.getElementById("buttonRight").addEventListener("touchend", ButtonUp, {p
 
 document.getElementById("buttonDown").addEventListener("touchstart", DownbuttonOnClick, {passive: true});
 document.getElementById("buttonDown").addEventListener("touchend", ButtonUp, {passive: true});
-
 
 var mySound = document.getElementById("sound");   
 
